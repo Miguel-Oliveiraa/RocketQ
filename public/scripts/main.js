04717b8c4f0f6089a00d886ee3ec24544fde0eda
migasrcf <miguellgomes64@gmail.com>
2021-07-11 15:57:46 -0300

    update room buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,31 @@ import Modal from "./modal.js";
 
 const modal = Modal();
 
-// pegar todos os botões que tem a classe check
-const checkButtons = document.querySelectorAll(".actions a.check");
+const modalTitle = document.querySelector(".modal h2");
+const modalDesc = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
 
-checkButtons.forEach(button => {
-  //   conferir se o butão recebeu click
-  button.addEventListener("click", event => {
-    // abrir modal
-    modal.open();
-  });
+// botao marcar como lida
+const checkButtons = document.querySelectorAll(".actions a.check");
+checkButtons.forEach((button) => {
+  button.addEventListener("click", handleClick);
 });
+
+// botão excluir
+const deleteButton = document.querySelectorAll(".actions a.delete");
+deleteButton.forEach((button) => {
+  button.addEventListener("click", (event) => handleClick(event, false));
+});
+
+function handleClick(event, check = true) {
+  const text = check ? "Marcar como lida" : "Excluir";
+
+  modalTitle.innerHTML = `${text} esta pergunta`;
+  modalDesc.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} essa pergunta?`;
+  modalButton.innerHTML = `sim, ${text.toLocaleLowerCase()}`;
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+  // abrir modal
+  modal.open();
+}
